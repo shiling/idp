7ea2051b4d2bc24fb4fd3e47bd749d040074958c
Tai Shi Ling <shiling.tai@gmail.com>
2014-03-05 13:41:13 +0800

    create categoriesService and productsService

--- foundation5/js/angular/app.js
+++ foundation5/js/angular/app.js
@@ -4,28 +4,78 @@
  * and open the template in the editor.
  */
 var app = angular.module('cmartApp', ['mm.foundation', 'webStorageModule']);
-app.directive('ngModelOnblur', function () {
-  return {
-      restrict: 'A',
-      require: 'ngModel',
-      priority: 1,
-      link: function (scope, element, attrs, ngModelCtrl) {
-          if (attrs.type === 'radio' || attrs.type === 'checkbox') { return; }
-          var update = function () {
-              scope.$apply(function () {
-                  ngModelCtrl.$setViewValue(element.val().trim());
-                  ngModelCtrl.$render();
-              });
-          };
-          element.off('input').off('keydown').off('change').on('focus', function () {
-              scope.$apply(function () {
-                  ngModelCtrl.$setPristine();
-              });
-          }).on('blur', update).on('keydown', function (e) {
-              if (e.keyCode === 13) {
-                  update();
-              }
-          });
-      }
-  };
+
+//retrieves categories from json file
+app.factory('categoriesService', function($http) {
+    var promise;
+    var categoriesService = {
+        async: function() {
+            if (!promise) {
+                promise = $http.get('./data/categories.json').then(function(response) {
+                    categories = [];
+                    $.each(response.data, function(i, e) {
+                        cat = $.extend(new Category, e); //convert object to Category
+                        cat.subcategories = [];
+                        $.each(e.subcategories, function(i, e2) {
+                            sub_cat = $.extend(new Category, e2); //convert object to Category
+                            cat.subcategories.push(sub_cat);
+                        });
+                        categories.push(cat);
+                    });
+                    return categories;
+                });
+            }
+            return promise;
+        }
+    };
+    return categoriesService;
+});
+
+//retrieves products from json file
+app.factory('productsService', function($http) {
+    var promise;
+    var productsService = {
+        async: function() {
+            if (!promise) {
+                promise = $http.get('./data/products.json').then(function(response) {
+                    products = [];
+                    $.each(response.data, function(i, e) {
+                        product = $.extend(new Product, e); //convert object to Product
+                        products.push(product);
+                    });
+                    return products;
+                });
+            }
+            return promise;
+        }
+    };
+    return productsService;
+});
+
+app.directive('ngModelOnblur', function() {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        priority: 1,
+        link: function(scope, element, attrs, ngModelCtrl) {
+            if (attrs.type === 'radio' || attrs.type === 'checkbox') {
+                return;
+            }
+            var update = function() {
+                scope.$apply(function() {
+                    ngModelCtrl.$setViewValue(element.val().trim());
+                    ngModelCtrl.$render();
+                });
+            };
+            element.off('input').off('keydown').off('change').on('focus', function() {
+                scope.$apply(function() {
+                    ngModelCtrl.$setPristine();
+                });
+            }).on('blur', update).on('keydown', function(e) {
+                if (e.keyCode === 13) {
+                    update();
+                }
+            });
+        }
+    };
 });
--- foundation5/js/angular/catelogControllers.js
+++ foundation5/js/angular/catelogControllers.js
@@ -1,57 +1,46 @@
 //CONTROLLERS
-function categoriesController($scope, $http){
-  $scope.categories = [];
-  
-  $scope.init = function(){
-      $http.get('./data/categories.json').success(function(data){
-          $.each(data, function(i,e){
-              cat = $.extend(new Category, e);  //convert object to Category
-              cat.subcategories = [];
-              $.each(e.subcategories, function(i,e2){
-                  sub_cat = $.extend(new Category, e2); //convert object to Category
-                  cat.subcategories.push(sub_cat);
-              });
-            $scope.categories.push(cat);
-            
-          });
-      });
-  };
+function categoriesController($scope, $http, categoriesService) {
+    $scope.categories = [];
+
+    $scope.init = function() {
+        //get categories data
+        categoriesService.async().then(function(data) {
+            $scope.categories = data;
+        });
+    };
 }
 
-function productsController($scope, $http, webStorage){
+function productsController($scope, $http, webStorage, productsService) {
     $scope.products = [];
     $scope.cart;
-    
-    $scope.init = function(){
-        //get products from json data file
-        $http.get('./data/products.json').success(function(data){
-            $.each(data, function(i,e){
-                product = $.extend(new Product, e); //convert object to Product
-                $scope.products.push(product);
-            });
+
+    $scope.init = function() {
+        //get product data
+        productsService.async().then(function(data) {
+            $scope.products = data;
         });
         //get cart from localstorage
-        if(webStorage.get("cart") === null){
-            webStorage.add("cart",new Cart());
+        if (webStorage.get("cart") === null) {
+            webStorage.add("cart", new Cart());
         }
         $scope.cart = $.extend(new Cart, webStorage.get("cart"));  //convert object to Cart
     };
-    
-    $scope.updateQuantity = function(product, quantity){
+
+    $scope.updateQuantity = function(product, quantity) {
         $scope.cart.updateQuantity(product, parseInt(quantity));
         webStorage.add("cart", $scope.cart);   //update localstorage
     };
-    
-    $scope.validateQuantity = function(product){
+
+    $scope.validateQuantity = function(product) {
         var intRegex = /^\d+$/;
         quantity = $scope.cart.getQuantity(product);
-        if(!intRegex.test(quantity)){   //not an integer, set to zero
-            $scope.updateQuantity(product,0);
+        if (!intRegex.test(quantity)) {   //not an integer, set to zero
+            $scope.updateQuantity(product, 0);
         }
         webStorage.add("cart", $scope.cart);   //update localstorage
     };
-    
-    $scope.getQuantity = function(product){
+
+    $scope.getQuantity = function(product) {
         return $scope.cart.getQuantity(product);
     };
 }
