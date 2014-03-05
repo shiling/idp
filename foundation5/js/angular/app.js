0d85f50c8688af7ab6f72fd6f9f25dd3427ba40c
Tai Shi Ling <shiling.tai@gmail.com>
2014-03-05 13:11:16 +0800

    add directive for ng-model-onblur

--- foundation5/js/angular/app.js
+++ foundation5/js/angular/app.js
@@ -3,4 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-angular.module('cmartApp', ['mm.foundation', 'webStorageModule']);
+var app = angular.module('cmartApp', ['mm.foundation', 'webStorageModule']);
+app.directive('ngModelOnblur', function () {
+  return {
+      restrict: 'A',
+      require: 'ngModel',
+      priority: 1,
+      link: function (scope, element, attrs, ngModelCtrl) {
+          if (attrs.type === 'radio' || attrs.type === 'checkbox') { return; }
+          var update = function () {
+              scope.$apply(function () {
+                  ngModelCtrl.$setViewValue(element.val().trim());
+                  ngModelCtrl.$render();
+              });
+          };
+          element.off('input').off('keydown').off('change').on('focus', function () {
+              scope.$apply(function () {
+                  ngModelCtrl.$setPristine();
+              });
+          }).on('blur', update).on('keydown', function (e) {
+              if (e.keyCode === 13) {
+                  update();
+              }
+          });
+      }
+  };
+});
