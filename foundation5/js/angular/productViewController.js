40cfc6f98460cf4b09ef5e67d08eb5b22ca52a6a
seankschua <sean.ksc@gmail.com>
2014-03-09 13:33:08 +0800

    Related function fixed

--- foundation5/js/angular/productViewController.js
+++ foundation5/js/angular/productViewController.js
@@ -1,22 +1,44 @@
 function retrieveProduct($scope, $http){
     $scope.products = [];
     $scope.product = null;
+    $scope.productFound = false;
     
     $scope.init = function(){
         $http.get('./data/products.json').success(function(data){
             $.each(data, function(i,e){
-                product = $.extend(new Product, e);
-                console.log(product.name);
+                currentProduct = $.extend(new Product, e);
+                $scope.products.push(currentProduct);
+                console.log(currentProduct.name);
                 console.log(decodeURI(window.location.search.replace( "?", "" )));
-                console.log( product.name === decodeURI(window.location.search.replace( "?", "" )));
-                if ( product.name === decodeURI(window.location.search.replace( "?", "" ))){
-                    $scope.product = product;
-                    return false;
-                } else {
-                    $scope.pname = decodeURI(window.location.search.replace( "?", "" )) + ' not found!'
+                console.log( currentProduct.name === decodeURI(window.location.search.replace( "?", "" )));
+                if ( !$scope.productFound && currentProduct.name === decodeURI(window.location.search.replace( "?", "" ))){
+                    $scope.product = currentProduct;
+                    $scope.productFound = true;
                 };
             });
         });
+    };
+    
+    $scope.related = function(products, product){
+        relatedProducts = [];
+        console.log("related function");
+        console.log(products);
+        console.log(product);
+        console.log(product.related);
+        for (relatedIndex in product.related){
+            relatedName = product.related[relatedIndex];
+            console.log(relatedName);
+            for (checkProductIndex in products){
+                checkProduct = products[checkProductIndex];
+                console.log(checkProduct.name + relatedName);
+                if (checkProduct.name === relatedName){
+                    relatedProducts.push(checkProduct);
+                    console.log("triggered!");
+                    break;
+                };
+            };
+        };
+        return relatedProducts;
     };
 }
 
